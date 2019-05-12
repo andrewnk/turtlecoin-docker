0b924d33828d5fb62ad769782f047ee3e4b98029
Andrew Kimball <andrewnk@comcast.net>
2019-05-12 09:27:17 -0400

    clean up

--- dockerfiles/turtlecoin-test-suite-ttyd/docker-entrypoint.sh
+++ dockerfiles/turtlecoin-test-suite-ttyd/docker-entrypoint.sh
@@ -2,24 +2,27 @@
 set -e
 
 #for when the ttyd ssl bug is fixed
-#ttyd --ssl --ssl-cert /ttyd/certs/server.crt --ssl-key /ttyd/certs/server.key --ssl-ca /ttyd/certs/ca.crt
+#ttyd --ssl --ssl-cert /ttyd/certs/server.crt \
+#   --ssl-key /ttyd/certs/server.key \
+#   --ssl-ca /ttyd/certs/ca.crt
 
-# in order to maintain a single session and not restart the
-# container on reload or crash we run the process with tmux
-set -- ttyd
+if [[ "$1" = 'ttyd' ]]; then
 
-if [[ ! -z "$WEB_USERNAME" && ! -z "$WEB_PASSWORD" ]]; then
-    set -- "$@" -c ${WEB_USERNAME}:${WEB_PASSWORD}
-fi
+    if [[ ! -z "$WEB_USERNAME" && ! -z "$WEB_PASSWORD" ]]; then
+        set -- "$@" -c ${WEB_USERNAME}:${WEB_PASSWORD}
+    fi
 
-set -- "$@" tmux new -A -s turtlecoin \
-    turtlecoin-test-suite
+    # in order to maintain a single session and not restart the
+    # container on reload or crash we run the process with tmux
+    set -- "$@" tmux new -A -s turtlecoin \
+        turtlecoin-test-suite
 
-if [ "$1" = 'ttyd' -a "$(id -u)" = '0' ]; then
-    find . \! -user turtlecoin -exec chown turtlecoin '{}' +
-    export PATH="$PATH:/home/turtlecoin/npm/bin"
-    export NODE_PATH="$NODE_PATH:/home/turtlecoin/npm/lib/node_modules"
-    exec su-exec turtlecoin "$0" "$@"
+    if [[ "$(id -u)" = '0' ]]; then
+        find . \! -user turtlecoin -exec chown turtlecoin '{}' +
+        export PATH="$PATH:/home/turtlecoin/npm/bin"
+        export NODE_PATH="$NODE_PATH:/home/turtlecoin/npm/lib/node_modules"
+        exec su-exec turtlecoin "$@"
+    fi
 fi
 
 exec "$@"
