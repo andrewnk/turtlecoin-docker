eafcc636b51cd535d1dfd55fdb87017b461d0ee3
Andrew Kimball <andrewnk@comcast.net>
2019-05-11 22:21:37 -0400

    fix runing tmux as non root user

--- dockerfiles/turtlecoin-test-suite-ttyd/docker-entrypoint.sh
+++ dockerfiles/turtlecoin-test-suite-ttyd/docker-entrypoint.sh
@@ -4,19 +4,21 @@ set -e
 #for when the ttyd ssl bug is fixed
 #ttyd --ssl --ssl-cert /ttyd/certs/server.crt --ssl-key /ttyd/certs/server.key --ssl-ca /ttyd/certs/ca.crt
 
-# in order to maintain a single session and not restart to container on reload or crash
-# we run the process with tmux
+# in order to maintain a single session and not restart the
+# container on reload or crash we run the process with tmux
 set -- ttyd
 
 if [[ ! -z "$WEB_USERNAME" && ! -z "$WEB_PASSWORD" ]]; then
     set -- "$@" -c ${WEB_USERNAME}:${WEB_PASSWORD}
 fi
 
-set -- "$@" tmux new -A -s turtlecoin-test-suite \
+set -- "$@" tmux new -A -s turtlecoin \
     turtlecoin-test-suite
 
-if [ "$1" = 'turtlecoin-test-suite' -a "$(id -u)" = '0' ]; then
+if [ "$1" = 'ttyd' -a "$(id -u)" = '0' ]; then
     find . \! -user turtlecoin -exec chown turtlecoin '{}' +
+    export PATH="$PATH:/home/turtlecoin/npm/bin"
+    export NODE_PATH="$NODE_PATH:/home/turtlecoin/npm/lib/node_modules"
     exec su-exec turtlecoin "$0" "$@"
 fi
 
---
+++ dockerfiles/turtlecoin-test-suite-ttyd/zshrc
@@ -0,0 +1,8 @@
+set -o allexport
+bindkey -e
+LC_CTYPE=en_US.UTF-8
+LANG=en_US.UTF-8
+
+autoload -Uz promptinit
+promptinit
+prompt bigfade
